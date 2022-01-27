caab7673c0cde46293e534b10111a3a32b1659fe
SameerAteeq <sameerateeq0@gmail.com>
2022-01-27 18:08:25 +0500

    Math-Methods

---
+++ day7-I.js
@@ -0,0 +1,49 @@
+// #############################        #59       MATH METHODS      #59      ################################# 
+
+//  NOTE : FOR USING THIS MATH METHODS WE HAVE TO PRINT FIRST "" MATH ""
+
+//<!-----------------------  1: Ceil  ----------------------->
+
+var a = Math.ceil(2.5);
+document.write(a + "<br>"); // This method give nearest upper value and value should be in decimal if we give negative value it gives nearest lower value
+
+
+//<!-----------------------  2: Floor (oppsite of ceil)  ----------------------->
+
+var a = Math.floor(2.5);
+document.write(a+ "<br>"); // This method give nearest lower value of given variable and value should be in decimal if we give negative value it gives nearest upper value
+
+//<!-----------------------  3: Round   ----------------------->
+
+var a = Math.round(-2.5);
+document.write(a+ "<br>"); // This method give nearest value of given variable and value should be in decimal if we give negative value it gives nearest lower value
+
+//<!-----------------------  4: Trunc   ----------------------->
+
+var a = Math.trunc(12.5);
+document.write(a+ "<br>"); // This method give the value which is before decimal point
+
+//<!-----------------------  5: Max   ----------------------->
+
+var a = Math.max(12, 13 , 15, 33, 1112);
+document.write(a+ "<br>"); //In This method we can give the multiple values and it shows the the highest value in the given variable
+
+//<!-----------------------  5: Min  ----------------------->
+
+var a = Math.min(12, 13 , 15, 33, 1112);
+document.write(a+ "<br>"); // In This method we can give the multiple values and it shows the lowest value in the given variable
+
+//<!-----------------------  6: Square root (sqrt) ----------------------->
+
+var a = Math.sqrt(64);
+document.write(a+ "<br>"); // In This method we can take sqare root of any number
+
+//<!-----------------------  6: Cube root (cbrt) ----------------------->
+
+var a = Math.cbrt(125);
+document.write(a+ "<br>"); // In This method we can take cube root of any number
+
+//<!-----------------------  6: Power of (pow) ----------------------->
+
+var a = Math.pow(4,3);
+document.write(a+ "<br>"); // In This method we can find power of any number first we will give base number then write its power.
