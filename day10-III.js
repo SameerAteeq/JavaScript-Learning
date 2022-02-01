9abfbc29ffc2506002715ce0d328024e67854ddf
SameerAteeq <sameerateeq0@gmail.com>
2022-02-01 21:25:06 +0500

    location object properties

---
+++ day10-III.js
@@ -0,0 +1,10 @@
+// ############################### Location Object Properties ##################################
+
+//location object is used to get  the value of URL
+
+
+//1.hash 2.protocol 3.host 4.hostname 5.href 6.origin 7.pathname 8.port 9.search 10. assign 12.reload 13.replace(replace will remove all previous pages)
+document.write(location.protocol);
+function newfunction(){
+    location.assign("https://www.google.com");
+}
